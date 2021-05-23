f322a8e607a2a969bee8b333638cf3b6fce7dfeb
unknown <dutt2630@gmail.com>
2021-05-23 15:37:40 +0530

    calculator updated

--- calculator/script.js
+++ calculator/script.js
@@ -1,119 +1,61 @@
+const display = document.getElementById('result');
+
 function dis(val){
-    const display = document.getElementById('result');
     display.value += val;
 }
 
+function expo(str){
+    str = str.split('Ex');
+    let s = str[0];
+    let p = str[1]
+    let x = (s*(Math.pow(10,p)))
+    display.value = x;
+}
+
 function solve(){
-    const display = document.getElementById('result');
+    if (display.value.includes('Ex')){
+        let c = display.value;
+        expo(c);
+    }
     let x = display.value;
     let y = eval(x);
     display.value = y; 
 }
 
+function del(){
+    let y = display.value;
+    y = y.slice(0, y.length - 1);
+    display.value = y;
+}
+
 function ac(){
-    const display = document.getElementById('result');
     display.value = '';
 }
 
 function xn(n){
-    const display = document.getElementById('result');
     display.value = Math.pow(display.value,n);
 }
 
-function sin(){
-    const display = document.getElementById('result');
-    display.value = Math.sin(display.value*((Math.PI)/180));
+function trigo(op){
+    display.value = Math[op](display.value);
 }
-function sinh(){
-    const display = document.getElementById('result');
-    display.value = Math.sinh(display.value);
-}
-function asin(){
-    const display = document.getElementById('result');
-    display.value = Math.asin(display.value);
-}
-
-function asinh(){
-    const display = document.getElementById('result');
-    display.value = Math.asinh(display.value);
-}
-
-function cos(){
-    const display = document.getElementById('result');
-    display.value = Math.cos(display.value);
-}
-
-function cosh(){
-    const display = document.getElementById('result');
-    display.value = Math.cosh(display.value);
-}
-
-function acos(){
-    const display = document.getElementById('result');
-    display.value = Math.acos(display.value);
-}
-
-function acosh(){
-    const display = document.getElementById('result');
-    display.value = Math.acosh(display.value);
-}
-
-function tan(){
-    const display = document.getElementById('result');
-    display.value = Math.tan(display.value);
-}
-
-function tanh(){
-    const display = document.getElementById('result');
-    display.value = Math.tanh(display.value);
-}
-
-function atan(){
-   
-   
-    const display = document.getElementById('result');
-    display.value = Math.atan(display.value);
-}
-
-function atanh(){
-    const display = document.getElementById('result');
-    display.value = Math.atanh(display.value);
-}
-
-function cot(){
-    const display = document.getElementById('result');
-    display.value = 1/Math.tan(display.value);
-
-}
-function coth(){
-    const display = document.getElementById('result');
-    display.value = 1/Math.tanh(display.value);
+function trigoIn(op){
+    display.value = 1/Math[op](display.value);
 }
 
 function log(){
-    const display = document.getElementById('result');
     display.value = Math.log10(display.value);
 }
 function ln(){
-    const display = document.getElementById('result');
     display.value = Math.log(display.value);
 }
 
 function nRoot(n){
-    const display = document.getElementById('result');
     display.value = Math.pow(display.value,1/n);
 }
 
 function prcnt(){
-    const display = document.getElementById('result');
     let x = display.value;
     let y = (eval(x)/100);
-    display.value = y;
-}
-
-function del(){
-    const display = document.getElementById('result');
-    let y = display.value;
-    y = y.slice(0, y.length - 1);
     display.value = y;
 }
